835aa26e507964d897912320109f1ee872d5216d
Can Öztürk <canozturk@Can-MacBook-Pro.home>
2021-02-07 19:00:32 +0300

    can

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,9 @@
 import UIKit
 import Flutter
+import GoogleMaps
+import Firebase
+import FirebaseCore
+import FirebaseDynamicLinks;
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +11,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyALnT7pxhQRDuQ3X5RdHEFfUbGtr4w7VL8")
+     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
